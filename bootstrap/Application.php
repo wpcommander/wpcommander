1255800d9ee1dbaf6362a961d89e0e4b07f9d383
MD AL AMIN <mdalaminbey@gmail.com>
2023-01-04 14:48:41 +0600

    Update Application.php

--- bootstrap/Application.php
+++ bootstrap/Application.php
@@ -7,7 +7,7 @@ use WpCommander\Application as WpCommanderApplication;
 class Application extends WpCommanderApplication
 {
     public static $instance, $config;
-    protected static $instances = [], $configs = [], $is_boot = false, $root_dir;
+    protected static $instances = [], $configs = [], $is_boot = false, $root_dir, $root_url;
 
     public function configuration(): array
     {
